ab049cb2ec255fa56286a42d7a51e0b2e171924b
Hunter Loftis <hunter@hunterloftis.com>
2014-11-15 11:25:44 -0600

    updated service template

--- services/template.js
+++ services/template.js
@@ -1,20 +1,38 @@
 var jackrabbit = require('jackrabbit');
 var logger = require('logfmt');
+var throng = require('throng');
+var cpus = require('os').cpus().length;
 
 var RABBIT_URL = process.env.CLOUDAMQP_URL || 'amqp://localhost';
 
-var broker = jackrabbit(RABBIT_URL, 1);
-broker.once('connected', create);
+throng(start, { workers: cpus, lifetime: Infinity });
 
-function create() {
-  broker.create('foo.get', serve);
-}
+function start() {
+  logger.log({ type: 'info', message: 'starting foo service' });
 
-function serve() {
-  logger.log({ type: 'info', message: 'serving foo' });
+  var broker = jackrabbit(RABBIT_URL, 1);
+  broker.once('connected', create);
+  process.once('uncaughtException', onError);
 
-  broker.handle('foo.get', function getFoo(message, reply) {
-    logger.log({ type: 'info', message: 'sending "Bar"' });
-    reply('Bar');
-  });
+  function create() {
+    broker.create('foo.get', serve);
+  }
+
+  function serve() {
+    logger.log({ type: 'info', message: 'serving foo' });
+
+    broker.handle('foo.get', function getWeather(message, reply) {
+      var timer = logger.time('foo.get').namespace(message);
+      setTimeout(function onFoo() {
+        timer.log();
+        reply('Bar');
+      }, 100);
+    });
+  }
+
+  function onError(err) {
+    logger.log({ type: 'error', service: 'foo', error: err, stack: err.stack || 'No stacktrace' }, process.stderr);
+    logger.log({ type: 'info', message: 'killing foo' });
+    process.exit();
+  }
 }
